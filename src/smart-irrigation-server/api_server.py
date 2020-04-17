8626dbf0dca70da9a001673b5e56608b9a565136
Stephen <knkp934@gmail.com>
2020-04-17 17:22:35 -0400

    Sensor and SensorData tables and relationships defined, updated ng-app to latest

--- src/smart-irrigation-server/api_server.py
+++ src/smart-irrigation-server/api_server.py
@@ -1,8 +1,36 @@
 import sys, json
+from datetime import datetime
 from flask import Flask
 from flask import send_from_directory
 from flask import render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__, static_url_path='')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pi_smart_irrigation.db'
+db = SQLAlchemy(app)
+
+# Models (put in another file shortly)
+class SensorData(db.Model):
+    data_id = db.Column(db.Integer, primary_key=True)
+    measured_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    sensor_data = db.Column(db.Float, nullable=False)
+    sensor_id = db.Column(db.Integer, db.ForeignKey('sensor.sensor_id'), nullable=False)
+    sensor = db.relationship('Sensor', 
+                backref=db.backref('sensor_data'))
+    def __repr__(self):
+        return '<val: %r>' % self.sensor_data
+
+
+class Sensor(db.Model):
+    sensor_id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(80), unique=True, nullable=False)
+    sensor_type = db.Column(db.String(80), unique=True, nullable=False)
+    # xLabels foreign key to SensorDataTable
+    # sensor_data foreign key to SensorDataTable
+    def __repr__(self):
+        return '<type: %r>' % self.label
+
+
 
 @app.route('/')
 @app.route('/<path>')
